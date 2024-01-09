d300812ef4ca5d29288e554833e2292da49b8ef0
Omar Badr <omarbadr938@gmail.com>
2024-01-09 14:45:54 +0100

    convert AddComment.jsx in functional component

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,35 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col, Container, Form, Button, Alert } from 'react-bootstrap';
-class AddComment extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      bookComment: {
-        comment: '',
-        rate: '1',
-        elementId: props.asin,
-      },
-    };
+const AddComment = (props) => {
+  const [bookComment, setBookComment] = useState({
+    comment: '',
+    rate: '1',
+    elementId: props.asin,
+  });
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
 
-  handleInputChange(property, value) {
-    this.setState({
-      bookComment: { ...this.state.bookComment, [property]: value },
-    });
-  }
+  //   this.state = {
+  //     bookComment: {
+  //       comment: '',
+  //       rate: '1',
+  //       elementId: props.asin,
+  //     },
+  //   };
 
-  async handleSubmit(e) {
+  //   this.handleInputChange = this.handleInputChange.bind(this);
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  // }
+
+  const handleInputChange = (property, value) => {
+    setBookComment({ ...bookComment, [property]: value });
+    // this.setState({
+    //   bookComment: { ...this.state.bookComment, [property]: value },
+    // });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const res = await fetch(
         'https://striveschool-api.herokuapp.com/api/comments',
         {
           method: 'POST',
-          body: JSON.stringify(this.state.bookComment),
+          body: JSON.stringify(bookComment),
           headers: {
             'Content-Type': 'application/json',
             Authorization:
@@ -39,75 +47,74 @@ class AddComment extends React.Component {
       );
 
       if (res.ok) {
-        this.setState({
-          bookComment: {
-            comment: '',
-            rate: '1',
-            elementId: this.props.asin,
-          },
+        setBookComment({
+          comment: '',
+          rate: '1',
+          elementId: props.asin,
         });
+        // this.setState({
+        //   bookComment: {
+        //     comment: '',
+        //     rate: '1',
+        //     elementId: this.props.asin,
+        //   },
+        // });
       } else {
         throw new Error('Errore nel salvataggio del commento');
       }
     } catch (e) {
       console.log('ERRORE: ', e);
     }
-  }
+  };
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <Form onSubmit={this.handleSubmit}>
-              <Form.Group>
-                <Form.Label>Comment</Form.Label>
-                <Form.Control
-                  type='text'
-                  placeholder='Insert your comment'
-                  value={this.state.bookComment.comment}
-                  onChange={(e) =>
-                    this.handleInputChange('comment', e.target.value)
-                  }
-                ></Form.Control>
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Rate</Form.Label>
-                <Form.Select
-                  aria-label='Rate'
-                  required
-                  value={this.state.bookComment.rate}
-                  onChange={(e) => {
-                    this.handleInputChange('rate', e.target.value);
-                  }}
-                >
-                  <option selected>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </Form.Select>
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>ASIN</Form.Label>
-                <Form.Control
-                  type='text'
-                  placeholder='ASIN'
-                  value={this.state.bookComment.elementId}
-                  onChange={(e) =>
-                    this.handleInputChange('elementId', e.target.value)
-                  }
-                ></Form.Control>
-              </Form.Group>
-              <Button variant='primary' type='submit' className='my-3'>
-                Add Comment!
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label>Comment</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Insert your comment'
+                value={bookComment.comment}
+                onChange={(e) => handleInputChange('comment', e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Rate</Form.Label>
+              <Form.Select
+                aria-label='Rate'
+                required
+                value={bookComment.rate}
+                onChange={(e) => {
+                  handleInputChange('rate', e.target.value);
+                }}
+              >
+                <option selected>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>ASIN</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='ASIN'
+                value={bookComment.elementId}
+                onChange={(e) => handleInputChange('elementId', e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Button variant='primary' type='submit' className='my-3'>
+              Add Comment!
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default AddComment;
