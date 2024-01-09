dade305c853727ae0434fcae42d6281317b33bdd
Omar Badr <omarbadr938@gmail.com>
2024-01-09 14:24:45 +0100

    convert ErrorAlert.jsx in functional component

--- src/components/ErrorAlert.jsx
+++ src/components/ErrorAlert.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Alert } from 'react-bootstrap';
 
-class ErrorAlert extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const ErrorAlert = () => {
+  return <Alert variant='danger'>Si è verificato un errore </Alert>;
+};
 
-  render() {
-    return <Alert variant='danger'>Si è verificato un errore </Alert>;
-  }
-}
+// class ErrorAlert extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return <Alert variant='danger'>Si è verificato un errore </Alert>;
+//   }
+// }
 
 export default ErrorAlert;
